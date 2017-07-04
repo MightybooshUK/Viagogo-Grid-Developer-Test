2ec419b7c29d2754cb8dfda9bbe5abfed3220e49
Oliver Dawson <oliver.dawson2015@my.ntu.ac.uk>
2017-07-04 20:35:14 +0100

    Added two new classes

--- GridDeveloperTest/src/griddevelopertest/GridDeveloperTest.java
+++ GridDeveloperTest/src/griddevelopertest/GridDeveloperTest.java
@@ -2,9 +2,12 @@
 package griddevelopertest;
 import java.util.*;
 
+
 /** 
  * @author Oliver
  */
+
+
 
 public class GridDeveloperTest {
 
@@ -16,18 +19,76 @@ public class GridDeveloperTest {
         //Make the grid
         int grid[][] = new int[21][21];
         
-        //Create a scanner
-        Scanner InputReader = new Scanner(System.in);
-        
-        int xCoord = -11, yCoord = -11, seperator = -1;
-        
-        System.out.println("Please Input Coordinates:");
-        String input = InputReader.nextLine();
-        
         
         
         //Testing
         System.out.println(randomGenerator.nextInt(10));
     }
     
+    Coordinates userInput()
+    {
+        //Create a scanner
+        Scanner InputReader = new Scanner(System.in);
+        //Declare variables
+        int xCoord = -11, yCoord = -11, seperator = -1;
+        //Get coordinates from user
+        System.out.println("Please Input Coordinates:");
+        String input = InputReader.nextLine();
+        
+        seperator = input.indexOf(',');
+        if(seperator == -1)
+        {
+            System.out.println("Please enter Coordinates in the formate X,Y ");            
+        }
+        else
+        {
+            xCoord = Integer.valueOf(input.substring(0,seperator));
+            yCoord = Integer.valueOf(input.substring(seperator+1));
+        }
+      return new Coordinates(xCoord, yCoord);  
+    }
+}
+
+final class Coordinates 
+{
+    private final int xCoord;
+    private final int yCoord;
+    
+    public Coordinates(int x, int y)
+    {
+        this.xCoord = x;
+        this.yCoord = y;
+    }
+    
+    public int getX()
+    {
+        return xCoord;
+    }
+    
+    public int getY()
+    {
+        return yCoord;
+    }
+}
+
+final class Event
+{
+    private final int ticketNumber;
+    private final float ticketPrice;
+    
+    public Event(int number, float price)
+    {
+        this.ticketNumber = number;
+        this.ticketPrice = price;
+    }
+    
+     public int getNumber()
+    {
+        return ticketNumber;
+    }
+    
+    public float getPrice()
+    {
+        return ticketPrice;
+    }
 }
