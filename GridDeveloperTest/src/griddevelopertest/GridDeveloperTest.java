6a4bdb981615b9e2a67dc9adc48312d708935841
Oliver Dawson <oliver.dawson2015@my.ntu.ac.uk>
2017-07-04 22:04:55 +0100

    Almost complete code

--- GridDeveloperTest/src/griddevelopertest/GridDeveloperTest.java
+++ GridDeveloperTest/src/griddevelopertest/GridDeveloperTest.java
@@ -10,76 +10,115 @@ import java.util.*;
 
 
 public class GridDeveloperTest {
+    
+    //Create a random number generator
+    private static Random randomGenerator = new Random();
+    //Store the 'world' grid
+    private static int grid[][] = new int[21][21];
+    //Create a list of events
+    private static ArrayList<Event> events = new ArrayList<Event>();
+    //User inputed coordinates
+    private static int xCoord = 0, yCoord = 0;
 
-    public static void main(String[] args) {
-
-        //Create a random number generator
-        Random randomGenerator = new Random();
-
-        //Make the grid
-        int grid[][] = new int[21][21];
+    public static void main(String[] args) 
+    {
+        GenerateSeed();
+         System.out.println(grid);
+        userInput();
+        
+        for(int i = 0;i < events.size() ; i++)
+        {
+            events.get(i).distance(xCoord, yCoord);
+        }
+        
+        Collections.sort(events);
         
         
         
         //Testing
-        System.out.println(randomGenerator.nextInt(10));
+        System.out.println(events.get(0).getId());
     }
     
-    Coordinates userInput()
+    public static void userInput()
     {
         //Create a scanner
         Scanner InputReader = new Scanner(System.in);
         //Declare variables
         int xCoord = -11, yCoord = -11, seperator = -1;
-        //Get coordinates from user
-        System.out.println("Please Input Coordinates:");
-        String input = InputReader.nextLine();
+        boolean validCoordinates = false;
         
-        seperator = input.indexOf(',');
-        if(seperator == -1)
+        while(!validCoordinates)
         {
-            System.out.println("Please enter Coordinates in the formate X,Y ");            
+            //Get coordinates from user
+            System.out.println("Please Input Coordinates:");
+            String input = InputReader.nextLine();
+            
+            //find the comma in the user's answer
+            seperator = input.indexOf(',');
+            if(seperator == -1)
+            {
+                //if there isn't a comma the input format is invalid
+                System.out.println("Please enter Coordinates in the format X,Y ");            
+            }
+            else
+            {
+                //if there is then take the integer value of the two strings either side of the comma
+                xCoord = Integer.valueOf(input.substring(0,seperator)) + 10;
+                System.out.println(xCoord);
+                yCoord = Integer.valueOf(input.substring(seperator+1)) + 10;
+                System.out.println(yCoord);
+            } 
+            
+            if(xCoord < 21 && xCoord > -1)
+            {
+                 if(yCoord < 21 && xCoord > -1)
+                 {
+                     validCoordinates = true;
+                 } 
+            }                        
         }
-        else
+    }
+    
+    public static void GenerateSeed()
+    {
+        int counter = 0;
+        for(int i = 0; i < 20; i++)
         {
-            xCoord = Integer.valueOf(input.substring(0,seperator));
-            yCoord = Integer.valueOf(input.substring(seperator+1));
+            for(int j = 0; j < 20; j++)
+            {
+                if(randomGenerator.nextInt(10) <= 1)
+                {
+                    grid[i][j] = counter;
+                    GenerateEvent(i, j);
+                    counter++;
+                }
+            }
         }
-      return new Coordinates(xCoord, yCoord);  
+    }
+    public static void GenerateEvent(int x, int y)
+    {
+        Event tempEvent = new Event(events.size(),x,y, randomGenerator.nextInt(101),randomGenerator.nextInt(100) + 1 + randomGenerator.nextFloat());
+        events.add(tempEvent);        
+        
     }
 }
 
-final class Coordinates 
+final class Event implements Comparable<Event>
 {
-    private final int xCoord;
-    private final int yCoord;
-    
-    public Coordinates(int x, int y)
-    {
-        this.xCoord = x;
-        this.yCoord = y;
-    }
-    
-    public int getX()
-    {
-        return xCoord;
-    }
-    
-    public int getY()
-    {
-        return yCoord;
-    }
-}
-
-final class Event
-{
-    private final int ticketNumber;
+    private final int ticketNumber, eventId, xCoord, yCoord;
     private final float ticketPrice;
     
-    public Event(int number, float price)
+    private int distanceTo;
+    
+    public Event( int eventId,int xCoord, int yCoord, int number, float price)
     {
+        this.eventId = eventId;
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
         this.ticketNumber = number;
         this.ticketPrice = price;
+        
+        
     }
     
      public int getNumber()
@@ -90,5 +129,33 @@ final class Event
     public float getPrice()
     {
         return ticketPrice;
+    }
+    
+    public int getId()
+    {
+        return eventId;
+    }  
+    
+    public void distance(int xCoord, int yCoord)
+    {
+        this.distanceTo = Math.abs(yCoord - this.yCoord) + Math.abs(xCoord - this.xCoord);        
+    }
+
+    @Override
+    public int compareTo(Event ev) 
+    {
+        if(this.distanceTo > ev.distanceTo)
+        {
+            return 1;
+        }
+        else if(this.distanceTo == ev.distanceTo)
+        {
+            return 0;
+        }
+        else 
+        {
+            return -1;
+        }
+        
     }
 }
